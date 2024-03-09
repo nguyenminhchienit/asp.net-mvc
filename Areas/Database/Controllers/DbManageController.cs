8848accc7e188654d6f46323ebb8038d0c1e5f28
NguyenMinhChien <minhchien03112003@gmail.com>
2024-03-09 09:13:59 +0700

    #3 Add entity framework

---
+++ Areas/Database/Controllers/DbManageController.cs
@@ -0,0 +1,46 @@
+﻿using aspmvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspmvc.Areas.Database.Controllers
+{
+    [Area("Database")]
+    [Route("/database-manage/[action]")]
+    public class DbManageController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public DbManageController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DeleteDb()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteDbAsync()
+        {
+            var success = await _dbContext.Database.EnsureDeletedAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Migrate()
+        {
+            await _dbContext.Database.MigrateAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace aspmvc.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            /*foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }*/
+        }
+
+    }
+}
