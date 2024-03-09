fdf15aeec249ac8b33fdd41865d9e97789cd38c1
NguyenMinhChien <minhchien03112003@gmail.com>
2024-03-09 20:06:14 +0700

    #5 Add Identity

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+﻿using App.Models;
 using aspmvc.Models;
 using aspmvc.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,64 @@ builder.Services.AddDbContext<AppDbContext> (options =>
     string connectString = builder.Configuration.GetConnectionString("appmvc");
     options.UseSqlServer(connectString);
 });
+
+
+//Dang ky dich vu Identity
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Thiết lập về Password
+    options.Password.RequireDigit = false; // Không bắt phải có số
+    options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+    options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+    options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+    options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+    options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+    // Cấu hình Lockout - khóa user
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // Khóa 5 phút
+    options.Lockout.MaxFailedAccessAttempts = 3; // Thất bại 3 lầ thì khóa
+    options.Lockout.AllowedForNewUsers = true;
+
+    // Cấu hình về User.
+    options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+
+    // Cấu hình đăng nhập.
+    options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+    options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+    options.SignIn.RequireConfirmedAccount = true;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/login/";
+    options.LogoutPath = "/logout/";
+    options.AccessDeniedPath = "/khongduoctruycap.html";
+});
+
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        var gconfig = builder.Configuration.GetSection("Authentication:Google");
+        options.ClientId = gconfig["ClientId"];
+        options.ClientSecret = gconfig["ClientSecret"];
+        // https://localhost:5001/signin-google
+        options.CallbackPath = "/dang-nhap-tu-google";
+    })
+    .AddFacebook(options =>
+    {
+        var fconfig = builder.Configuration.GetSection("Authentication:Facebook");
+        options.AppId = fconfig["AppId"];
+        options.AppSecret = fconfig["AppSecret"];
+        options.CallbackPath = "/dang-nhap-tu-facebook";
+    })
+    ;
 
 builder.Services.AddSingleton(typeof(ProductService), typeof(ProductService));
 builder.Services.AddSingleton<PlanetService>();
@@ -34,7 +95,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+app.UseAuthentication(); // xac thuc nguoi dung
+app.UseAuthorization(); // Xac dinh quyen truy cap
 
 app.MapControllerRoute(
     name: "default",
